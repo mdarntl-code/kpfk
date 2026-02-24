eb0a0a14f1c21626a2ae71a73b4f1e994b6facf1
mdarntl-code <bmartamartab@gmail.com>
2026-02-24 13:24:38 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = "kpfk";
-const basePath = isGithubActions ? `/${repo}` : "";
-
 const nextConfig: NextConfig = {
-    basePath,
     typescript: {
         ignoreBuildErrors: true,
     },
